d1836cb0db8422009195fa3d4c00591f9836c665
andre <andre@andre-IPMH110P>
2021-06-16 07:21:06 -0300

    Primeiro Commit

---
+++ AdminLTE/src/main/java/com/adminlte/admin/AdminLteApplication.java
@@ -0,0 +1,13 @@
+package com.adminlte.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminLteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminLteApplication.class, args);
+	}
+
+}
